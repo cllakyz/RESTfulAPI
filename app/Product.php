1429e6231a4a698c9a40086814c89109780f4575
Celal <cllakyz@hotmail.com>
2019-05-05 23:47:45 +0300

    Model transformer implementing

--- app/Product.php
+++ app/Product.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Transformers\ProductTransformer;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -13,6 +14,8 @@ class Product extends Model
     const UNAVAILABLE_PRODUCT   = 'unavailable';
 
     protected $dates = ['deleted_at'];
+
+    public $transformer = ProductTransformer::class;
 
     protected $fillable = [
         'name',
